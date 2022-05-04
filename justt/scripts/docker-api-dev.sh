3f9a90bc6d036c216bc4a5aeafaf4fd5383274c7
Sergey Lukin <contact@sergeylukin.com>
2022-05-04 08:23:35 +0300

    infra: declare DB details in .env configuration

--- justt/scripts/docker-api-dev.sh
+++ justt/scripts/docker-api-dev.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
-# Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
+# Need to explicit pass DATABASE_URL here as first argument and re-declare it,
+# otherwise migration doesn't work
+export DATABASE_URL="$1"
 # Run migrations
-DATABASE_URL="postgresql://prisma:prisma@postgres:5432/prisma?schema=public" npm run prisma:generate
-DATABASE_URL="postgresql://prisma:prisma@postgres:5432/prisma?schema=public" npx prisma migrate deploy
+npm run prisma:generate
+npx prisma migrate deploy
 # start app
-DATABASE_URL="postgresql://prisma:prisma@postgres:5432/prisma?schema=public" npm run seed
-DATABASE_URL="postgresql://prisma:prisma@postgres:5432/prisma?schema=public" npm run serve:api
+npm run seed
+npm run serve:api

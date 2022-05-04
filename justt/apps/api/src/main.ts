822d008aa7878765c04ed4918d25f2194f6f73bd
Sergey Lukin <contact@sergeylukin.com>
2022-05-04 20:13:29 +0300

    Add swagger for api

--- justt/apps/api/src/main.ts
+++ justt/apps/api/src/main.ts
@@ -5,13 +5,25 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
+
+const configureSwagger = (app) => {
+  const options = new DocumentBuilder()
+    .setTitle('Production Angular API')
+    .setDescription('REST API for the Production Angular course')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  configureSwagger(app);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(

cf5fe507fadb0e406953ef9b5629950d27abce76
Sergey Lukin <contact@sergeylukin.com>
2022-05-07 17:13:11 +0300

    infra: deploy cluster to Render

--- justt/apps/api/src/main.ts
+++ justt/apps/api/src/main.ts
@@ -25,9 +25,10 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   configureSwagger(app);
   const port = process.env.PORT || 3333;
-  await app.listen(port);
+  const host = process.env.HOST || '0.0.0.0';
+  await app.listen(port, host);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://${host}:${port}/${globalPrefix}`
   );
 }
 
